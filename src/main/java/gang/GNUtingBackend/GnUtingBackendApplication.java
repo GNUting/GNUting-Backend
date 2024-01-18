90e9856673ba8b930d05dd818b103b0030987beb
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-01-19 02:09:35 +0900

    setting: 프로젝트 초기 설정

---
+++ src/main/java/gang/GNUtingBackend/GnUtingBackendApplication.java
@@ -0,0 +1,13 @@
+package gang.GNUtingBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnUtingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnUtingBackendApplication.class, args);
+	}
+
+}
