e7c15f667dd0128ade3d402c2de1f8a87dae8aa2
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-02-03 01:46:33 +0900

    feat(User): User Entity 생성
    성별을 관리하는 Gender, 역할을 관리하는 UserRole -> enum으로 관리

--- src/main/java/gang/GNUtingBackend/user/domain/User.java
+++ src/main/java/gang/GNUtingBackend/user/domain/User.java
@@ -1,6 +1,15 @@
 package gang.GNUtingBackend.user.domain;
 
+import gang.GNUtingBackend.user.domain.enums.Gender;
+import gang.GNUtingBackend.user.domain.enums.UserRole;
+import java.time.LocalDate;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,5 +24,54 @@ import org.hibernate.annotations.DynamicUpdate;
 @AllArgsConstructor
 @DynamicInsert
 @DynamicUpdate
-public class User {
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 이메일
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // 비밀번호
+    @Column(nullable = false)
+    private String password;
+
+    // 이름
+    @Column(nullable = false)
+    private String name;
+
+    // 전화번호
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    // 성별
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    // 생년월일
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    // 닉네임
+    @Column(nullable = false)
+    private String nickname;
+
+    // 학과
+    @Column(nullable = false)
+    private String department;
+
+    // 학번
+    @Column(nullable = false)
+    private String studentId;
+
+    // 프로필 이미지
+    @Column(nullable = false)
+    private String profileImage;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
 }
