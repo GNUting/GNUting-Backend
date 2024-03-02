14eefd72926dde26e5bdcd909ba3a16195ff2b57
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-02 17:18:26 +0900

    refactor(BoardSearchResultDto): 학번 출력 형식 변경

--- src/main/java/gang/GNUtingBackend/board/dto/BoardSearchResultDto.java
+++ src/main/java/gang/GNUtingBackend/board/dto/BoardSearchResultDto.java
@@ -1,15 +1,15 @@
 package gang.GNUtingBackend.board.dto;
 
+import java.awt.image.TileObserver;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class BoardSearchResultDto {
 
-    private Long BoardId;
+    private Long boardId;
 
     // 게시글 제목
     private String title;
@@ -19,4 +19,11 @@ public class BoardSearchResultDto {
 
     // 작성자 학번
     private String studentId;
+
+    public BoardSearchResultDto(Long boardId, String title, String department, String studentId) {
+        this.boardId = boardId;
+        this.title = title;
+        this.department = department;
+        this.studentId = studentId.substring(2, 4) + "학번";
+    }
 }
