e4bb6553edda406ad0830e47d24a3c1768f16a77
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-04 20:37:49 +0900

    refactor(DepartmentController): @Operation, @Parameter를 통해 swagger 문서 내 설명 작성

--- src/main/java/gang/GNUtingBackend/department/controller/DepartmentController.java
+++ src/main/java/gang/GNUtingBackend/department/controller/DepartmentController.java
@@ -3,8 +3,9 @@ package gang.GNUtingBackend.department.controller;
 import gang.GNUtingBackend.department.domain.Department;
 import gang.GNUtingBackend.department.service.DepartmentService;
 import gang.GNUtingBackend.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import java.util.List;
-import javax.naming.Name;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +26,8 @@ public class DepartmentController {
      * @return
      */
     @GetMapping("/search-department")
-    public ResponseEntity<ApiResponse<List<Department>>> searchDepartments(@RequestParam("name") String name) {
+    @Operation(summary = "회원가입 시 학과 검색 API", description = "회원가입 시 자신의 학과를 검색합니다.")
+    public ResponseEntity<ApiResponse<List<Department>>> searchDepartments(@RequestParam("name") @Parameter(description = "학과 이름") String name) {
         List<Department> departments = departmentService.searchDepartments(name);
 
         return ResponseEntity.ok()
