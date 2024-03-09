69d83d12458e2d737a99d1ac5c36dc1974af5c26
isoProject <jj99526@naver.com>
2024-03-09 20:10:35 +0900

    feat: 특정글 이미지 반환 , 시간 반환

--- src/main/java/gang/GNUtingBackend/board/dto/BoardResponseDto.java
+++ src/main/java/gang/GNUtingBackend/board/dto/BoardResponseDto.java
@@ -7,6 +7,8 @@ import gang.GNUtingBackend.user.domain.enums.Gender;
 import gang.GNUtingBackend.user.dto.UserSearchResponseDto;
 import lombok.*;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -24,9 +26,11 @@ public class BoardResponseDto {
     private Status status;
     private Gender gender;
     private int inUserCount;
-
+    private String time;
 
     public static BoardResponseDto toDto(Board board, List<UserSearchResponseDto> user) {
+
+        String elapsedTime = getElapsedTime(board.getCreatedDate());
 
         return BoardResponseDto.builder()
                 .id(board.getId())
@@ -37,7 +41,28 @@ public class BoardResponseDto {
                 .user(BoardWriterInfoDto.toDto(board.getUserId()))
                 .gender(board.getGender())
                 .inUserCount(board.getInUserCount())
+                .time(elapsedTime)
                 .build();
+    }
+
+
+    //반환할때 시간설정 메소드
+    private static String getElapsedTime(LocalDateTime createdTime) {
+        LocalDateTime currentTime = LocalDateTime.now();
+        Duration duration = Duration.between(createdTime, currentTime);
+        long minutes = duration.toMinutes();
+
+        if (minutes < 1) {
+            return "방금 전";
+        } else if (minutes < 60) {
+            return minutes + "분 전";
+        } else if (minutes < 1440) {
+            long hours = minutes / 60;
+            return hours + "시간 전";
+        } else {
+            long days = minutes / 1440;
+            return days + "일 전";
+        }
     }
 
 }
--- src/main/java/gang/GNUtingBackend/board/dto/BoardWriterInfoDto.java
+++ src/main/java/gang/GNUtingBackend/board/dto/BoardWriterInfoDto.java
@@ -12,12 +12,14 @@ public class BoardWriterInfoDto {
     private String nickname;
     private String department;
     private String studentId;
+    private String image;
 
     public static BoardWriterInfoDto toDto(User user){
        return BoardWriterInfoDto.builder()
                 .department(user.getDepartment())
                 .nickname(user.getNickname())
                 .studentId(user.getStudentId().substring(2,4)+"학번")
+                .image(user.getProfileImage())
                 .build();
     }
 
