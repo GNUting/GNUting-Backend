bcb6f5b8f883966bb70c78846d33c3b6d813cf5b
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-14 05:30:23 +0900

    feat(Token): redis를 사용하여 refreshToken, accessToken을 사용하기 위한 Token 생성

---
+++ src/main/java/gang/GNUtingBackend/user/domain/Token.java
@@ -0,0 +1,28 @@
+package gang.GNUtingBackend.user.domain;
+
+import java.util.concurrent.TimeUnit;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Token {
+
+    @Id
+    private Long id;
+
+    private String refreshToken;
+
+    private String accessToken;
+
+    @TimeToLive(unit = TimeUnit.MILLISECONDS)
+    private long expiration;
+
+
+}
