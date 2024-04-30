048d158c0f546bfc2e9d2eb99123f4e4880fe3aa
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-04-30 17:13:08 +0900

    feat: 내 게시글 보기, 게시판 글 보기, 게시글 검색 reponse에 작성 시간 설정하여 반환하도록 필드 추가

--- src/main/java/gang/GNUtingBackend/board/dto/BoardShowAllResponseDto.java
+++ src/main/java/gang/GNUtingBackend/board/dto/BoardShowAllResponseDto.java
@@ -3,6 +3,8 @@ package gang.GNUtingBackend.board.dto;
 import gang.GNUtingBackend.board.entity.Board;
 import gang.GNUtingBackend.board.entity.enums.Status;
 import gang.GNUtingBackend.user.domain.enums.Gender;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import lombok.*;
 
 @Getter
@@ -21,9 +23,13 @@ public class BoardShowAllResponseDto {
     private Gender gender;
     private BoardWriterInfoDto user;
     private int inUserCount;
+    private String time;
 
 
     public static BoardShowAllResponseDto toDto(Board board) {
+
+        String elapsedTime = getElapsedTime(board.getCreatedDate());
+
         return BoardShowAllResponseDto.builder()
                 .id(board.getId())
                 .title(board.getTitle())
@@ -31,9 +37,28 @@ public class BoardShowAllResponseDto {
                 .status(board.getStatus())
                 .gender(board.getGender())
                 .inUserCount(board.getInUserCount())
+                .time(elapsedTime)
                 .user(BoardWriterInfoDto.toDto(board.getUserId()))
                 .build();
     }
 
+    //반환할때 시간설정 메소드
+    private static String getElapsedTime(LocalDateTime createdTime) {
+        LocalDateTime currentTime = LocalDateTime.now();
+        Duration duration = Duration.between(createdTime, currentTime);
+        long minutes = duration.toMinutes();
+
+        if (minutes < 1) {
+            return "방금 전";
+        } else if (minutes < 60) {
+            return minutes + "분 전";
+        } else if (minutes < 1440) {
+            long hours = minutes / 60;
+            return hours + "시간 전";
+        } else {
+            long days = minutes / 1440;
+            return days + "일 전";
+        }
+    }
 }
 
